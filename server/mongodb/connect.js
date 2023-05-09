ff48d438c268e8a14707fe74e28b4b35681d449f
OmGholap <omsagargholap@gmail.com>
2023-05-09 10:08:52 +0530

    layout updated

---
+++ server/mongodb/connect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const connectDB = (url) => {
+    mongoose.set("strictQuery", true);
+
+    mongoose
+        .connect(url)
+        .then(() => console.log("MongoDB connected"))
+        .catch((error) => console.log(error));
+};
+
+export default connectDB;
