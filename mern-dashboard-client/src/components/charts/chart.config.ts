2a805f85b4df85a9c50b0b64080acce1f1304c34
Aryan Apte <aryan.kindle9000@gmail.com>
2023-05-14 16:29:42 +0530

    charts

--- mern-dashboard-client/src/components/charts/chart.config.ts
+++ mern-dashboard-client/src/components/charts/chart.config.ts
@@ -6,17 +6,8 @@ export const TotalRevenueSeries = [
     //     data: [183, 124, 115, 85, 143],
     // },
     {
-<<<<<<< HEAD
         name: "This Month",
         data: [95, 84, 72, 44, 108],
-=======
-        name: "Last Month",
-        data: [183, 124, 115, 85, 143, 143, 96],
-    },
-    {
-        name: "Running Month",
-        data: [95, 84, 72, 44, 108, 108, 47],
->>>>>>> 4a0808c2eb7e1f651f5454a809d892cef93c2a7f
     },
 ];
 
